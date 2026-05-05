e6f5c0660f51e865b5b87a3f60c206f0143c5d91
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-05 12:03:16 +0200

    complete lesson 06

---
+++ 02_first_steps/05_restaurant/after/includes/header.inc.php
@@ -0,0 +1,26 @@
+<!-- 
+ There are still some issues: the title and background image don't change.
+This will be fixed in the next lesson.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/simple.css" />
+    <link rel="stylesheet" href="./styles/custom.css" />
+    <title>Culinary Cove &bull; Our mission</title>
+</head>
+<body>
+  <header class="header-with-background" style="background-image: url('images/pexels-engin-akyurt-1435904.jpg'); ">
+    <h1>Culinary Cove</h1>
+    <p>Your sanctuary for exceptional flavors</p>
+    <nav>
+      <a href="our-mission.php">Our mission</a>
+      <a href="ingredients.php">Ingredients</a>
+      <!-- <a href="mailto:culinary-cove@example.com">Send us a E-mail</a> -->
+    </nav>
+  </header>
+
+  <main>

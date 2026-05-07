fe26f717ac4dc8b7e552f37cdbddf1d0632dcc29
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-07 12:57:28 +0200

    add exercise 4

---
+++ 03_php_essential/03_code_exercise_4.php
@@ -0,0 +1,13 @@
+<!--Loyalty Points Evaluation (Part 1)-->
+
+<?php
+
+// The variable $loyaltyPoints will be available to you
+if ($loyaltyPoints >= 3000) {
+    if ($loyaltyPoints >= 6000) {
+        echo "You can spend 6000 Loyalty Points for a discount of 15%.";
+    } else echo "You can spend 3000 Loyalty Points for a discount of 5%.";
+} else echo "You have fewer than 3000 Loyalty Points. No discount is available.";
+
+?>
+

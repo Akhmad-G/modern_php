662df22887c2583a7ef7a90bb31dcbb74e8a89fb
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-07 11:55:59 +0200

    add exercise

---
+++ 03_php_essential/03_code_exercise_3.php
@@ -0,0 +1,15 @@
+<!--Online Shop Prices-->
+
+<?php
+
+// Assume $price is already set
+
+$discount = $price / 100 * 30;
+$priceAfterDiscount = $price - $discount - 10;
+$tax = $priceAfterDiscount / 100 * 20;
+$finalPrice = $priceAfterDiscount + $tax;
+$loyalty = $finalPrice * 100;
+
+echo "After applying discounts and taxes, the item's price is reduced from \${$price} to \${$finalPrice}, and you've earned $loyalty loyalty points.";
+
+?>

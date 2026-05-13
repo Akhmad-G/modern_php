04ae6da50b50868710646df1b0a06df71a1fc82b
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-13 11:04:11 +0200

    Add array exercises 10: An attempt to solve the problem using a different approach. Positioning error: when a name appears in two lists, the index cannot be calculated correctly.

--- 04_arrays/04_coding_exercise_10.php
+++ 04_arrays/04_coding_exercise_10.php
@@ -13,55 +13,57 @@
   <h1 style="text-align: center">Coding Exercise 10</h1>
   <pre>
     <?php
-    $waitingList = [
-      'Alex Reed',
-      'Blake Jordan',
-      'Casey Smith',
-      'Drew Alex',
-      'Elliot Ford',
-      'Finley Harper',
-      'Jordan Kay',
-      'Kim Lee',
-      'Liam Park',
-      'Morgan Drew'
-    ];
+//    $waitingList = [
+//      'Alex Reed',
+//      'Blake Jordan',
+//      'Casey Smith',
+//      'Drew Alex',
+//      'Elliot Ford',
+//      'Finley Harper',
+//      'Jordan Kay',
+//      'Kim Lee',
+//      'Liam Park',
+//      'Morgan Drew'
+//    ];
+    $waitingList = ['Eva Grant', 'Ian Hope', 'Olivia Jane'];
 
-    $waitingList = array_unique($waitingList);
-    $priorityParticipants = ['Jordan Kay', 'Sam Taylor', 'Zane Pryor'];
+    $priorityParticipants = [];
 
+    $finalList = array_merge($priorityParticipants, $waitingList);
+//    $finalList = array_unique($finalList);
+    var_dump($finalList);
     $finalAttendees = array();
     $backupCandidates = array();
-    foreach ([...$priorityParticipants, ...$waitingList] as $name) {
+    foreach ($finalList as $name) {
       if (count($finalAttendees) < 5) {
-        if (!in_array($name, $finalAttendees)) {
-          $finalAttendees[] = $name;
-        }
+        if (in_array($name, $finalAttendees)) continue;
+        $finalAttendees[] = $name;
       } elseif (count($backupCandidates) < 3) {
-        if (!in_array($name, $backupCandidates) && !in_array($name, $finalAttendees)) {
-          $backupCandidates[] = $name;
-          echo "\nHey $name, we want to inform you that you are one of our backup candidates. 🥳" . "\n";
-        }
+        if (in_array($name, $backupCandidates) or in_array($name, $finalAttendees)) continue;
+        $backupCandidates[] = $name;
+        echo "\nHey $name, we want to inform you that you are one of our backup candidates. 🥳";
       } else break;
     }
+
     sort($finalAttendees);
     sort($backupCandidates);
 
-//    var_dump($finalAttendees, $backupCandidates);
+    var_dump($finalAttendees, $backupCandidates);
+    var_dump(array_unique(['Dylan Marsh', 'Jake Frost', 'Julia Stream', 'Ava Stone', 'Dylan Marsh', 'Emma Lake', 'Grace Hill', 'Henry Cole', 'Kyle Brook', 'Lily Snow', 'Mason Cliff', 'Nora Field', 'Sophia Forest']));
 
     $individualName = 'Kim Lee';
-    $individualStatus ='';
+    $individualStatus = '';
 
     if (in_array($individualName, $finalAttendees)) {
       $individualStatus = "Final Attendee";
     } elseif (in_array($individualName, $backupCandidates)) {
       $individualStatus = "Backup Candidate";
-    } elseif (in_array($individualName, $waitingList)) {
-      $position = array_search($individualName, $waitingList) + 1;
-      foreach ($waitingList as $name) {
-        if (in_array($name, $finalAttendees) or in_array($name, $backupCandidates)) $position--;
-      }
+    } elseif (in_array($individualName, $finalList)) {
+      $position = array_search($individualName, $finalList) - 7;
       $individualStatus = "Waiting, position $position";
-    } else $individualStatus = "Not found";
+    } elseif (count($finalList) > 8 ) {
+      $individualStatus = "Not found";
+    }
 
     echo "\n" . $individualName . " you are in " . $individualStatus;
 

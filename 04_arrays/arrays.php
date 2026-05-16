2dfb9b55a29796f706e6b38d2cfd5ec5d5f97605
Akhmad-G <akhmad.gazimuhammadilav@outlook.com>
2026-05-16 14:25:24 +0200

    refactor: clean up project structure

--- 04_arrays/arrays.php
+++ 04_arrays/arrays.php
@@ -13,10 +13,10 @@
   <h1>Kapitel 4: Arrays</h1>
   <main>
     <ul>
-      <li><a href="04_coding_exercise_7.php">Coding Exercise 7</a></li>
-      <li><a href="04_coding_exercise_8.php">Coding Exercise 8</a></li>
-      <li><a href="04_coding_exercise_9.php">Coding Exercise 9</a></li>
-      <li><a href="04_coding_exercise_10.php">Coding Exercise 10</a></li>
+      <li><a href="coding_exercise_7.php">Coding Exercise 7</a></li>
+      <li><a href="coding_exercise_8.php">Coding Exercise 8</a></li>
+      <li><a href="coding_exercise_9.php">Coding Exercise 9</a></li>
+      <li><a href="coding_exercise_10.php">Coding Exercise 10</a></li>
     </ul>
   </main>
 

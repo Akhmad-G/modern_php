126d5225f4753805c7a60baeabf751d1cb9fbd29
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-19 12:59:10 +0200

    wip: continue section 7 project

--- 07_project_image_gallery/gallery.php
+++ 07_project_image_gallery/gallery.php
@@ -5,5 +5,17 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
+<main class="gallery-container">
+  <!--  --><?php //foreach ($imageTitles AS $imageName => $imageTitle) : ?>
+  <div class="gallery-item">
+    <img src="images/1B1A9657.jpg" alt="Snowy Mountain Range">
+    <p class="description"><a href="../index.html">Snowy Mountain Range</a></p>
+  </div>
+
+  <div class="gallery-item">
+    <img src="images/IMG_0219.jpg" alt="Snowy Range">
+    <p class="description"><a href="">Snowy Range</a></p>
+  </div>
+</main>
 
 <?php include './views/footer.php'; ?>
--- 07_project_image_gallery/views/header.php
+++ 07_project_image_gallery/views/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/simple.css" />
-    <link rel="stylesheet" type="text/css" href="../styles/custom.css" />
+    <link rel="stylesheet" type="text/css" href="./styles/custom.css" />
     <title>Image gallery</title>
 </head>
 <body>

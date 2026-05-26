b9db8dd1af7421fe29aa72b809ee6e6115a0d1a1
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-26 12:05:05 +0200

    feat: finish coding_exercise_14.php

---
+++ 08_advanced_concepts/coding_exercise_14.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+  >
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/simple.css">
+  <title>Coding Exercise</title>
+</head>
+<body>
+  <h1 style="text-align: center">Coding Exercise 14</h1>
+  <pre>
+    <?php
+
+    require_once "examples/inc/functions.inc.php";
+
+    $emailContent = "Subject: Unlock Your Potential with Us!\n\nDear Alex,\n\nWe hope this message finds you well.\n\nQuote of the Month:\n\nDr. Albert Szent-Györgyi: 'Innovation is seeing what everybody has seen and thinking what nobody has thought.'\n\nBest wishes,\nYour Discovery Network Team\nP.S. Don't miss our special announcement next month!";
+
+    $contentArray = explode("\n\n", $emailContent);
+    //    var_dump($contentArray);
+
+    $quoteIndex = array_search('Quote of the Month:', $contentArray) + 1;
+    //    if ($quoteIndex) echo $contentArray[$quoteIndex] . "\n";
+    //    else echo "Quote not found";
+
+    $author = explode(": ", $contentArray[$quoteIndex])[0];
+    $quote = explode(": ", $contentArray[$quoteIndex])[1];
+    $contentArray[$quoteIndex] = $quote . " - " . $author;
+
+    $modifiedEmailContent = implode("\n\n", $contentArray);
+
+    ?>
+  </pre>
+
+</body>
+</html>

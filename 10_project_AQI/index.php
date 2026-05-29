03a17067001748d599207f0fdc6874da609ff53f
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-29 12:41:33 +0200

    feat: section 10 project first step

--- 10_project_AQI/index.php
+++ 10_project_AQI/index.php
@@ -2,10 +2,16 @@
 
 <?php require __DIR__ . "/views/header.inc.php" ?>
 
-<pre><?php
+<?php $dataFromJSON = json_decode(file_get_contents("./data/index.json"), true); ?>
 
-  echo "Air Quality Project"
-
-?></pre>
+<ul>
+  <?php foreach ($dataFromJSON as $cityData) : ?>
+    <li>
+      <a href="city.php?<?php echo http_build_query(["city" => $cityData["city"]]) ?>">
+        <?php echo e($cityData["city"] . ", " . $cityData["country"] . " (" . $cityData["flag"] . ")") ?>
+      </a>
+    </li>
+  <?php endforeach; ?>
+</ul>
 
 <?php require __DIR__ . "/views/footer.inc.php" ?>

a293ffd2002844e7c154a3da0b9ef44c07d6b9e4
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-29 11:39:31 +0200

    chore: initialize section 10 project

---
+++ 10_project_AQI/index.php
@@ -0,0 +1,11 @@
+<?php require __DIR__ . "/inc/functions.inc.php" ?>
+
+<?php require __DIR__ . "/views/header.inc.php" ?>
+
+<pre><?php
+
+  echo "Air Quality Project"
+
+?></pre>
+
+<?php require __DIR__ . "/views/footer.inc.php" ?>

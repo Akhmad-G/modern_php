de9e9cd62331a51860a19f41e1b1889576759c18
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-05-29 12:51:04 +0200

    refactor: section 10 project first step

--- 10_project_AQI/index.php
+++ 10_project_AQI/index.php
@@ -1,14 +1,23 @@
-<?php require __DIR__ . "/inc/functions.inc.php" ?>
+<?php
+
+require __DIR__ . "/inc/functions.inc.php";
+
+$cities = json_decode(
+  file_get_contents(__DIR__ . "/data/index.json"),
+  true
+);
+
+?>
 
 <?php require __DIR__ . "/views/header.inc.php" ?>
 
-<?php $dataFromJSON = json_decode(file_get_contents("./data/index.json"), true); ?>
-
 <ul>
-  <?php foreach ($dataFromJSON as $cityData) : ?>
+  <?php foreach ($cities as $city) : ?>
     <li>
-      <a href="city.php?<?php echo http_build_query(["city" => $cityData["city"]]) ?>">
-        <?php echo e($cityData["city"] . ", " . $cityData["country"] . " (" . $cityData["flag"] . ")") ?>
+      <a href="city.php?<?php echo http_build_query(["city" => $city["city"]]) ?>">
+        <?php echo e($city["city"]); ?>,
+        <?php echo e($city["country"]); ?>
+        (<?php echo e($city["flag"]); ?>)
       </a>
     </li>
   <?php endforeach; ?>

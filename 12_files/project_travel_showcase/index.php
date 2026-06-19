c8ed28a58a98f6c3d3a4ec8ee03dd8b2741a7a83
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-06-19 12:51:55 +0200

    refactor: improve section 12 code

--- 12_files/project_travel_showcase/index.php
+++ 12_files/project_travel_showcase/index.php
@@ -10,47 +10,56 @@
   <title>Travel Showcase</title>
 </head>
 <body>
-  <?php require_once __DIR__ . "/inc/functions.inc.php"; ?>
-
-  <?php
-    $handle = opendir(__DIR__ . "/images");
-
-    $images = [];
-    $allowedImageExtensions = [
-      "jpg",
-      "jpeg",
-      "png"
-    ];
-    while (($currentFile = readdir($handle)) !== false) {
-      if ($currentFile === "." || $currentFile === "..") continue;
-      $extension = pathinfo($currentFile, PATHINFO_EXTENSION);
-
-      if (!in_array($extension, $allowedImageExtensions)) continue;
-      else {
-        $images[] = $currentFile;
-      }
-    }
-
-    $imageDescriptions = [];
-    foreach ($images as $image) {
-      $name = pathinfo($image, PATHINFO_FILENAME);
-      if (file_exists(__DIR__ . "/images/" . $name . ".txt")) {
-        $imageDescriptions[$image] = explode("\n", file_get_contents(__DIR__ . "/images/" . $name . ".txt"));
-      }
-    }
-    //      var_dump($imageDescriptions);
-
-    closedir($handle);
-  ?>
-
+  <header><h1>Automatic Image List</h1></header>
   <main>
+    <?php
+      function e($value) {
+        return htmlspecialchars($value, ENT_QUOTES, "UTF-8");
+      }
+
+
+      $handle = opendir(__DIR__ . "/images");
+
+      $images = [];
+      $allowedImageExtensions = [
+        "jpg",
+        "jpeg",
+        "png"
+      ];
+      while (($currentFile = readdir($handle)) !== false) {
+        if ($currentFile === "." || $currentFile === "..") continue;
+        $extension = pathinfo($currentFile, PATHINFO_EXTENSION);
+
+        if (!in_array($extension, $allowedImageExtensions)) continue;
+        else {
+          $images[] = $currentFile;
+        }
+      }
+
+      $imageDescriptions = [];
+      foreach ($images as $image) {
+        $name = pathinfo($image, PATHINFO_FILENAME);
+        if (file_exists(__DIR__ . "/images/" . $name . ".txt")) {
+          $imageDescriptions[$image] = explode("\n", file_get_contents(__DIR__ . "/images/" . $name . ".txt"));
+        }
+      }
+      //      var_dump($imageDescriptions);
+
+      closedir($handle);
+    ?>
+
     <?php foreach ($imageDescriptions as $image => $text): ?>
+    <figure>
       <h3><?php echo e($text[0]); ?></h3>
       <img src="<?php echo "./images/" . rawurlencode($image) ?>" alt="<?php echo e($text[0]) ?>">
-      <?php for ($paragraphNum = 1; $paragraphNum < count($text); $paragraphNum++): ?>
-        <p><?php echo e($text[$paragraphNum]) ?></p>
-      <?php endfor; ?>
-    <?php endforeach; ?>
+      <figcaption>
+        <?php for ($paragraphNum = 1; $paragraphNum < count($text); $paragraphNum++): ?>
+          <p><?php echo e($text[$paragraphNum]) ?></p>
+        <?php endfor; ?>
+      </figcaption>
+      <?php endforeach; ?>
+    </figure>
+
   </main>
 </body>
 </html>

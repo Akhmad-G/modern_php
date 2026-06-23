84de9d2071c86d2d1fdc17763ac42182d896d870
Akhmed Gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-06-23 09:46:42 +0200

    refactor: add e() function for all h3

--- 13_intro_to_mariaDB/index.php
+++ 13_intro_to_mariaDB/index.php
@@ -1,7 +1,22 @@
 <?php require_once "../header.inc.php"; ?>
-<h3><?php echo e(array_keys($topics)[12])?></h3>
+<h3><?php echo e(array_keys($topics)[12]) ?></h3>
 
 <h4 style="text-align: center;">The entire project was built using phpMyAdmin.</h4>
-
-  </body>
+<ul>
+  <h5>Database access:</h5>
+  <li>in XAMPP:
+    <ol>
+      <li><a href="http://localhost/dashboard">localhost/dashboard -></a> "phpMyAdmin"</li>
+    </ol>
+  </li>
+  <li>in <a href="https://addons.ddev.com/addons/ddev/ddev-phpmyadmin">DDEV</a>
+    <ol>
+      <li>open the terminal and enter the following commands:</li>
+      <li>ddev add-on get ddev/ddev-phpmyadmin</li>
+      <li>ddev restart</li>
+      <li>ddev phpmyadmin</li>
+    </ol>
+  </li>
+</ul>
+</body>
 </html>
